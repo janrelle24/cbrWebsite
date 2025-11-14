3cd3e05bad6635fbe09bd966c69f772cfa74889c
janrelle24 <lubianojanrelle@gmail.com>
2025-11-14 14:27:15 +0800

    create calendar design

--- script.js
+++ script.js
@@ -25,3 +25,36 @@ document.addEventListener("DOMContentLoaded", () =>{
     showSlide(slideIndex);
     setInterval(nextSlide, 4000);
 });
+
+document.addEventListener("DOMContentLoaded", () =>{
+    const monthYear = document.getElementById("month-year");
+    const daysContainer = document.getElementById("days");
+
+    const months = [
+        'JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE', 
+        'JULY', 'AUGUST', 'SEPTEMBER', 'OCTOBER', 'NOVEMBER', 'DECEMBER'
+    ];
+    let currentDate =new Date();
+    let today = new Date();
+
+    function renderCalendar(date){
+        const year = date.getFullYear();
+        const month = date.getMonth();
+        const firstDay = new Date(year, month, 1).getDay();
+        const lastDay = new Date(year, month + 1, 0).getDate();
+
+        monthYear.textContent = `${months[month]} ${year}`;
+
+        daysContainer.innerHTML = '';
+        //current months date
+        for(let i = 1; i <= lastDay; i++){
+            const dayDiv = document.createElement('div');
+            dayDiv.textContent = i;
+            if(i === today.getDate() && month === today.getMonth() && year === today.getFullYear()){
+                dayDiv.classList.add('today');
+            }
+            daysContainer.appendChild(dayDiv);
+        }
+    }
+    renderCalendar(currentDate);
+});
