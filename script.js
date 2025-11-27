6ab4b000498e6821dd08a64272fd12e93ca55164
janrelle24 <lubianojanrelle@gmail.com>
2025-11-27 19:56:43 +0800

    done fixing the padding-top of each page for screen size 1024

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-/*document.addEventListener("DOMContentLoaded", function () {
-    const sidebar = document.getElementById("sidebar");
-    const hamburger = document.getElementById("hamburger-btn");
-    const overlay = document.getElementById("overlay");
-
-    hamburger.addEventListener("click", () => {
-        sidebar.classList.toggle("active");
-        overlay.classList.toggle("active");
-    });
-
-    overlay.addEventListener("click", () => {
-        sidebar.classList.remove("active");
-        overlay.classList.remove("active");
-    });
-}); */
-
-
 document.addEventListener("DOMContentLoaded", () =>{
     let slideIndex = 0;
     const slides = document.querySelectorAll(".carosel .slide");
